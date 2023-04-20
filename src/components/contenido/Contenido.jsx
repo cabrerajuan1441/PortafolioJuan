eb7df79ee662b941f363b07b307d84fe5860f28c
cabrerajuan1441 <cabreraj1441@outlook.es>
2023-04-20 16:27:58 -0300

    nuevas modificaciones

--- src/components/contenido/Contenido.jsx
+++ src/components/contenido/Contenido.jsx
@@ -1,29 +1,70 @@
 import React from "react";
 import styles from "./Contenido.module.css";
 
-const Contenido = () => {
+export const Contenido = () => {
   return (
     <>
       <div className={styles.contenido}>
-        <div>
-          <h1 className={styles.desarrollador}>
-            <>
-              Cabrera Juan,
-              <br /> Front End Developer y <br /> Maquetador Web
-            </>
-          </h1>
-        </div>
         <div className={styles.sobreMi}>
-          <h2>
-            Hola! soy Juan, Front-End Developer en desarrollo,en busca de mis
-            primeras experiencias en el mundo IT, estudiante de la plataforma
-            Soy Henry, y autodidacta en Udemy.
-          </h2>
+          <h1 className={styles.desarrollador}>Front End Developer</h1>
         </div>
+        <h2 className={styles.sobreMiLetra}>
+          Hola! Soy Front-End Developer en desarrollo, en busca de mis primeras
+          experiencias en el mundo IT, estudiante de la plataforma Soy Henry, y
+          autodidacta en Udemy.
+        </h2>
       </div>
-      <div></div>
     </>
   );
 };
 
-export default Contenido;
+export const Proyectos = () => {
+  return (
+    <>
+      <div className={styles.proyectos}>
+        <h3 id="proyectos" className={styles.misProyectos}>
+          Mis Proyectos
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export const Conocimientos = () => {
+  return (
+    <>
+      <div className={styles.Conocimientos}>
+        <h3 id="conocimientos" className={styles.misConocimientos}>
+          Mis Conocimientos
+        </h3>
+      </div>
+      <div className={styles.containerImagenes}>
+        <img
+          className={styles.conocimientosImagenes}
+          alt="HTML"
+          src={require("./imagens/html-5.png")}
+        ></img>
+        <img
+          className={styles.conocimientosImagenes}
+          alt="CSS"
+          src={require("./imagens/css-3.png")}
+        ></img>
+        <img
+          className={styles.conocimientosImagenes}
+          alt="REACT"
+          src={require("./imagens/react.png")}
+        ></img>
+        <img
+          className={styles.conocimientosImagenes}
+          alt="JavasCript"
+          src={require("./imagens/java-script.png")}
+        ></img>
+        <img
+          className={styles.conocimientosImagenes}
+          alt="Bootstrap"
+          src={require("./imagens/bootstrap.png")}
+        ></img>
+      </div>
+    </>
+  );
+};
