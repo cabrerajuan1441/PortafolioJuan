d2e7c30ca385f6a29ac9316f6b705a44b392f31f
cabrerajuan1441 <cabreraj1441@outlook.es>
2023-04-22 17:19:57 -0300

    nuevos cambios

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div></div>
-      <div></div>
+      <div>
+        <p className={styles.footP}>Creado por: Cabrera Juan Pablo</p>
+      </div>
+      <div>
+        {" "}
+        <img
+          className={styles.footImg}
+          alt="Whatsapp"
+          src={require("./whatsapp.png")}
+        ></img>
+        <img
+          className={styles.footImg}
+          alt="Linkedin"
+          src={require("./linkedin.png")}
+        ></img>
+        <img
+          className={styles.footImg}
+          alt="GitHub"
+          src={require("./github.png")}
+        ></img>
+        <img
+          className={styles.footImg}
+          alt="FlechaArr"
+          src={require("./flecha-hacia-arriba.png")}
+        ></img>
+      </div>
     </footer>
   );
 };
